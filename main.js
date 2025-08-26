dd8ba1bf9522304ff471b2fb5dc8618a490687aa
Nathiele Palavecini <145784922+nathielepalavecini@users.noreply.github.com>
2025-08-26 08:25:33 -0300

    ff

--- main.js
+++ main.js
@@ -22,3 +22,7 @@ function aumentaTamanho(){
 }
 
 const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'
+const numeros = '0123456789'
+const simbolos = '!@#$%&?/+-=*'
