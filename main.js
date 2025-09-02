87909a271b0b9ce5908a1310583fd9bdf93f0d3c
Nathiele Palavecini <145784922+nathielepalavecini@users.noreply.github.com>
2025-09-02 08:04:18 -0300

    uk

--- main.js
+++ main.js
@@ -37,6 +37,23 @@ const simbolos = '!@#$%&?/+-=*';
 geraSenha();
 
 function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+
+    if (checkbox[2].checked) {
+        alfabeto = alfabeto + numeros;
+    }
+
+    if (checkbox[3].checked) {
+        alfabeto = alfabeto + simbolos;
+    }
+
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random() * letrasMaiusculas.length;
