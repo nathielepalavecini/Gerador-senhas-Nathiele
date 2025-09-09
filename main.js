248c796742b4f7a39e6d5370b880856708f569f2
Nathiele Palavecini <145784922+nathielepalavecini@users.noreply.github.com>
2025-09-09 10:25:23 -0300

    n n

--- main.js
+++ main.js
@@ -59,18 +59,17 @@ function geraSenha() {
         let numeroAleatorio = Math.random() * alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
         senha = senha + alfabeto[numeroAleatorio];
-   }
+    }
     campoSenha.value = senha;
     classificaSenha();
 }
-function classificaSenha(){
-    forcaSenha.classList.remove('fraca','media','forte');
-    if (tamanhoSenha > 11){
-        forcaSenha.classList.add('.forte');
-
-    } else if (tamanhoSenha>5 && tamanhoSenha<12){
+function classificaSenha() {
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if (tamanhoSenha > 11) {
+        forcaSenha.classList.add('forte');
+    } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
         forcaSenha.classList.add('media');
-    } else if (tamanhoSenha <=5){
+    } else if (tamanhoSenha <= 5) {
         forcaSenha.classList.add('fraca');
     }
 }
