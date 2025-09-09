5f14f3f4be973f80875f64a1bd81563f4dd8522d
Nathiele Palavecini <145784922+nathielepalavecini@users.noreply.github.com>
2025-09-09 10:23:33 -0300

    mmmm

--- main.js
+++ main.js
@@ -68,5 +68,9 @@ function classificaSenha(){
     if (tamanhoSenha > 11){
         forcaSenha.classList.add('.forte');
 
+    } else if (tamanhoSenha>5 && tamanhoSenha<12){
+        forcaSenha.classList.add('media');
+    } else if (tamanhoSenha <=5){
+        forcaSenha.classList.add('fraca');
     }
 }
